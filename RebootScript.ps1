cc0cfe222fbdf753bc948a648f95103c55b600bd
Dayan Raduychev <81255013+DayanRaduychev@users.noreply.github.com>
2021-11-19 16:22:06 +0000

    Created reboot script.

---
+++ RebootScript.ps1
@@ -0,0 +1,16 @@
+Add-Type -AssemblyName PresentationFramework
+
+$rebootFilePath = "C:\Macnamara\reboot.txt" 
+$rebootFile = Get-Content $rebootFilePath 
+$prompt = "New security software has been installed which requires a restart of this computer to complete. Can we restart now? If not, we'll ask again in 1 hour"
+
+if ($rebootFile -match "1") { 
+    $answer = [System.Windows.MessageBox]::Show($prompt, "Macnamara Reboot Prompt", "YesNo", "Warning")
+    if ($answer-eq "Yes") { 
+        $rebootFile -replace "1","0" | Set-Content -Path $rebootFilePath
+        Restart-Computer -Force
+    }
+} else { 
+    #Exit 0 
+    Write-host "Reboot not required."
+}
